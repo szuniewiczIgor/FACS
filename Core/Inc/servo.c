1dc273ad69082a066a6886cc937a3eab1ff06e32
szuni <szuni@DESKTOP-C8P6CP2>
2026-04-18 21:04:20 +0200

    feat: Added servo angle calculation

---
+++ Core/Inc/servo.c
@@ -0,0 +1,20 @@
+/*
+ * servo.c
+ * SERVO: MG90S
+ *  Created on: Apr 18, 2026
+ *      Author: szuniewiczIgor
+ */
+#include "servo.h"
+extern TIM_HandleTypeDef htim4; //allows use htim4
+
+void SetServoAngle(uint8_t angle){
+	if (angle > 180){
+		angle = 180;
+	}
+	uint16_t min_pulse = 575; //0 degree
+	uint16_t max_pulse = 2500; // 180 degree
+
+	uint16_t pulse = ((uint32_t)angle * (max_pulse - min_pulse) / 180) + min_pulse; //formula for pulse. angle is variable
+
+	__HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_1, pulse);
+}
---
+++ Core/Inc/servo.h
@@ -0,0 +1,15 @@
+/*
+ * servo.h
+ *
+ *  Created on: Apr 18, 2026
+ *      Author: szuniewiczIgor
+ */
+
+#ifndef INC_SERVO_H_
+#define INC_SERVO_H_
+#include "main.h"
+
+void SetServoAngle(uint8_t angle);
+
+
+#endif /* INC_SERVO_H_ */
